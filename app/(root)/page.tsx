d5ae6425aeeca7b4239c7f90617400917a1ed087
techieadi4703 <techie.adi47@gmail.com>
2026-01-20 03:43:20 +0530

    feat: add app layout with header, navigation, and user dropdown

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button"
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <Button>CLick me</Button>
+    <div className="flex min-h-screen home-wrapper">
+      Home
     </div>
     
   )
